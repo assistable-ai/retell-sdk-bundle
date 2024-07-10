7e97ad04b28fb4d00684b93486c564dbf5607eda
assistable-ai <166671192+assistable-ai@users.noreply.github.com>
2024-07-10 05:34:57 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
   mode: 'production',
   resolve: {
     fallback: {
-      crypto: require.resolve('crypto-browserify')
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
+      vm: require.resolve('vm-browserify')
     }
   }
 };
